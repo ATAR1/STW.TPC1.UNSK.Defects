bee0f3e3d0ab1967873bfd0cdfd432270f28036d
ATAR1 <thexenosmilu@gmail.com>
2018-05-17 15:32:24 +0500

    1

--- STW.TPC1.UNSK.Defects/LoadDataCommand.cs
+++ STW.TPC1.UNSK.Defects/LoadDataCommand.cs
@@ -1,6 +1,8 @@
 ﻿using STW.TPC1.UNSK.Defects.DbModel;
 using STW.TPC1.UNSK.Defects.MDT6DbModel;
+using STW.TPC1.UNSK.Defects.ScanerDbModel;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
 
@@ -24,10 +26,11 @@ namespace STW.TPC1.UNSK.Defects
 
         public void Execute(object parameter)
         {
+            var tubes = new List<TubeInfo>();
             _mainWindowViewModel.Tubes.Clear();
             using (var mdt6Context = new MDT6DBEntities())
             {
-                var tubes = mdt6Context.Tubes.Include("Melt.Standart").Include("Melt.Typesize").OrderByDescending(ti => ti.tCreatedDate).Take(2000).ToList()
+                tubes.AddRange( mdt6Context.Tubes.Include("Melt.Standart").Include("Melt.Typesize").OrderByDescending(ti => ti.tCreatedDate).Take(2000).ToList()
                     .Select(ti => {
                         var typeSizeString = ti.Melt.Typesize.AsString.Replace(" ","").Replace('x', '*').Replace('х', '*').Replace('.', ',');
                         var diameterString = typeSizeString.Substring(0, typeSizeString.IndexOf('*'));
@@ -49,11 +52,45 @@ namespace STW.TPC1.UNSK.Defects
                             WorkArea = "ТО 1",
                             DefectMap = ti.s_map_defects
                         };
-                    });
-                foreach (var tube in tubes)
-                {
-                    _mainWindowViewModel.Tubes.Add(tube);
-                }
+                    }));
+               
+
+            }
+
+            using (var scanerContext = new ScanerTubeInfoEntities())
+            {
+                tubes.AddRange(scanerContext.ScanerTubeInfoes.Include("Defects").Where(ti=>ti.TubeStatus==0).OrderByDescending(ti => ti.Date).Take(2000).ToList()
+                    .Select(ti =>{
+                        var typeSizeString = ti.TypeSize.Replace(" ", "").Replace('x', '*').Replace('х', '*').Replace('.', ',');
+                        var diameterString = typeSizeString.Substring(0, typeSizeString.IndexOf('*'));
+                        var thicknessString = typeSizeString.Substring(typeSizeString.IndexOf('*') + 1);
+                        double diameter = -1;
+                        double thickness = -1;
+                        Double.TryParse(diameterString, out diameter);
+                        Double.TryParse(thicknessString, out thickness);
+                        var tubeNumStr = ti.TubeNum;
+                        int tubeNum=0;
+                        Int32.TryParse(tubeNumStr, out tubeNum);
+                        return new DbModel.ScanerTubeInfo()
+                        {
+                            Date = ti.Date,
+                            Defectoscope = "Сканер",
+                            Diameter = diameter,
+                            Thickness = thickness,
+                            Level = (int)ti.TubeStatus,
+                            Melt = ti.Melt,
+                            Standart = ti.Standart,
+                            TubeNum = tubeNum,
+                            WorkArea = "ТО 1",
+                            DefectMap = String.Join(" ", ti.Defects.Select(d => $"{d.Name} : {d.Value}"))
+                        };
+                    }));
+
+            }
+
+            foreach (var tube in tubes)
+            {
+                _mainWindowViewModel.Tubes.Add(tube);
             }
         }
         
